fe4549d51ea2c88b06c88bfe4bbc539b633a9d9a
Nikolay Grinko <ngrink.io@gmail.com>
2024-03-27 19:39:05 +0300

    adds task filtration to "ls" command

--- cmd/ls.go
+++ cmd/ls.go
@@ -10,7 +10,11 @@ var lsCmd = &cobra.Command{
 	Short: "List tasks",
 	Long:  "List tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		core.Tasks.RenderAll()
+		if len(args) == 1 {
+			core.Tasks.RenderByScope(args[0])
+		} else {
+			core.Tasks.RenderAll()
+		}
 	},
 }
 
--- core/task.render.go
+++ core/task.render.go
@@ -8,7 +8,7 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
-func (t *TaskContainer) RenderAll() {
+func NewTable() *tablewriter.Table {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"#", "Done", "Description", "Scope", "Age"})
 	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
@@ -16,8 +16,35 @@ func (t *TaskContainer) RenderAll() {
 	table.SetColumnAlignment([]int{3, 1, 3, 3, 3})
 	table.SetBorders(tablewriter.Border{Left: false, Right: false, Top: false, Bottom: false})
 
+	return table
+}
+
+func (t *TaskContainer) RenderAll() {
+	table := NewTable()
+
 	fmt.Println()
 	for idx, task := range t.Data {
+		table.Append([]string{
+			strconv.Itoa(idx),
+			task.GetStatusMark(),
+			task.Description,
+			task.Scope,
+			task.GetAge(),
+		})
+	}
+
+	table.Render()
+	fmt.Println()
+}
+
+func (t *TaskContainer) RenderByScope(scope string) {
+	table := NewTable()
+
+	fmt.Println()
+	for idx, task := range t.Data {
+		if task.Scope != scope {
+			continue
+		}
 		table.Append([]string{
 			strconv.Itoa(idx),
 			task.GetStatusMark(),
